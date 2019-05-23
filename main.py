1f49ee6d0c3f1a4c143c0e051b2526a73beec475
John Christopher Allwein <john.allwein57@gmail.com>
2019-05-22 23:26:00 -0400

    general: add DEBUG flag and format source

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ from rcalendar import EventCalendar
 from ical import CalendarManager
 from reddit import RedditManager
 
+
+DEBUG = False
+
+
 def __init__():
     # Download links to /r/Troy .ics files from the google calendars
     TROY_ART = "https://calendar.google.com/calendar/ical/a7hd2r4rrosgqnmeq3rlgnfrg0%40group.calendar.google.com/public/basic.ics"
@@ -14,7 +18,7 @@ def __init__():
     #TROY_KIDS = "https://calendar.google.com/calendar/ical/5qneb2ti8iuevupniijsibusdo%40group.calendar.google.com/public/basic.ics"
     TROY_MUSIC = "https://calendar.google.com/calendar/ical/s83fkkrfbsks0mph5k836nsmhs%40group.calendar.google.com/public/basic.ics"
     TROY_OTHER = "https://calendar.google.com/calendar/ical/go2vrfg91b62mvglq3hqu45pnc%40group.calendar.google.com/public/basic.ics"
-    
+
     # Download rcalendar files
     print("Downloading rcalendar files...")
     CalendarManager.downloadCalendarFile(TROY_ART, "art.ics")
@@ -22,90 +26,93 @@ def __init__():
     #CalendarManager.downloadCalendarFile(TROY_KIDS, "kids.ics")
     CalendarManager.downloadCalendarFile(TROY_MUSIC, "music.ics")
     CalendarManager.downloadCalendarFile(TROY_OTHER, "other.ics")
-    
-    
+
     # Begin parsing
     artCal = EventCalendar("art.ics")
     artEvents = artCal.parseEvents()
-    
+
     del artCal
-    
+
     festivalsCal = EventCalendar("festivals.ics")
     festivalEvents = festivalsCal.parseEvents()
-    
+
     del festivalsCal
-    
+
     #kidsCal = EventCalendar("kids.ics")
     #kidsEvents = kidsCal.parseEvents()
-    
+
     #del kidsCal
-    
+
     musicCal = EventCalendar("music.ics")
     musicEvents = musicCal.parseEvents()
-    
+
     otherCal = EventCalendar("other.ics")
     otherEvents = otherCal.parseEvents()
-    
+
     del otherCal
-    
+
     # Prepare post
     text = 'Hello! Here are the Troy events for this week, organized by category. \n\n# Art Events\n\n******'
-    
+
     aEvents = computeEvents(artEvents)
     fEvents = computeEvents(festivalEvents)
     #kEvents = computeEvents(kidsEvents)
     mEvents = computeEvents(musicEvents)
     oEvents = computeEvents(otherEvents)
-    
+
     text += parseEvents(aEvents)
     text += "\n\n# Festival Events\n\n******"
-    text += parseEvents(fEvents) 
-    #text += "\n\n# Kids Events\n\n******"
-    #text += parseEvents(kEvents) 
+    text += parseEvents(fEvents)
+    # text += "\n\n# Kids Events\n\n******"
+    #text += parseEvents(kEvents)
     text += "\n\n# Music Events\n\n******"
-    text += parseEvents(mEvents) 
+    text += parseEvents(mEvents)
     text += "\n\n# Other Events\n\n******"
     text += parseEvents(oEvents)
-    
+
     text += "\nI am a bot, and this action was performed automatically."
-    
-    print (text)
-    
+
+    print(text)
+
     f = open("log.txt", 'w')
-    
+
     f.write(text)
-    
+
     reddit = setupReddit()
-    # Post to reddit
-    post = reddit.post("Troy", "Weekly Events for " + CalendarManager.getMonth() + "/" + str(CalendarManager.getDay()) + "/" + CalendarManager.getYear(), text)
-    reddit.crosspost(post, "rpi")
-    
+
+    if not DEBUG:
+        # Post to reddit
+        post = reddit.post("Troy", "Weekly Events for " + CalendarManager.getMonth() +
+                           "/" + str(CalendarManager.getDay()) + "/" + CalendarManager.getYear(), text)
+        reddit.crosspost(post, "rpi")
+
+
 def setupReddit():
     # Find credential file
     creds = open('reddit.cfg', 'r')
-    
+
     data = creds.readlines()
-    
+
     cId = ''
     cSecret = ''
     userAgent = ''
     username = ''
     password = ''
-    
+
     for line in data:
         if line.startswith("#"):
             continue
-        
+
         key = line.split("=")[0]
         value = line.split('=')[1]
-        
+
         if key == 'client_id':
             cId = value
             continue
         if key == 'client_secret':
             cSecret = value
             continue
-        if key == 'user_agent': 
+        if key == 'user_agent':
             userAgent = value
             continue
         if key == 'username':
@@ -114,16 +121,16 @@ def setupReddit():
         if key == 'password':
             password = value
             continue
-        
+
         print("Unrecognized key: " + key)
-        
+
     return RedditManager(cId, cSecret, userAgent, username, password)
-    
+
 
 def parseEvents(eventList):
     postText = "\n"
     hasApplied = False
-    
+
     for event in sorted(eventList):
         hasApplied = False
         for e in eventList.get(event):
@@ -134,7 +141,7 @@ def parseEvents(eventList):
                 hasApplied = True
             postText += "\n\n### " + e.get_summary()
             postText += "\n\n###  DESCRIPTION: \n" + e.get_description()
-            
+
             # Parse time
             hour = 0
             minutes = "0"
@@ -145,7 +152,7 @@ def parseEvents(eventList):
                 pass
 
             timeStr = ''
-            
+
             if hour > 12:
                 hour -= 12
                 timeStr = str(hour) + ":" + minutes + " PM"
@@ -153,38 +160,38 @@ def parseEvents(eventList):
                 timeStr = "12:" + minutes + " PM"
             else:
                 timeStr = str(hour) + ":" + minutes + " AM"
-                
+
             postText += "\n\n###  TIME: " + timeStr
             postText += "\n\n###  ADDRESS: " + e.get_address()
             postText += "\n### -- END OF EVENT --"
         postText += "\n"
     return postText
-            
+
 
 def computeEvents(eventList):
     calendar = dict()
-    
+
     for event in eventList:
         dateStr = str(event.get_start())
-        
+
         # Find year / month / day
         year = dateStr[0:4]
         month = dateStr[5:7]
         day = int(dateStr[8:10])
-        
-        # Throw out events not within a week of the future 
+
+        # Throw out events not within a week of the future
         if day - CalendarManager.getDay() > 0 and day - CalendarManager.getDay() <= 7:
             pass
         else:
             continue
-        
+
         # Make sure it is part of this year & month
         if not CalendarManager.getYear() == year:
             continue
-        
+
         if not CalendarManager.getMonth() == month:
             continue
-        
+
         try:
             # Oh noes
             arr = calendar[day]
@@ -192,20 +199,18 @@ def computeEvents(eventList):
             print("Multiple events in one day!")
         except KeyError:
             arr = list()
-            
+
             arr.append(event)
             calendar[day] = arr
 
-        
-        
         print("year: " + year)
         print("month: " + month)
         # print("day: " + day)
-        
+
         print("date: " + dateStr)
         print("today:" + CalendarManager.getDateString())
-    
-    return calendar    
-    
-    
+
+    return calendar
+
+
 __init__()
